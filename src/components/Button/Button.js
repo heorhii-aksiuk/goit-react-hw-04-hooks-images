275ad1277b06a8ff1d773253a4c466dced1483c7
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-18 16:06:33 +0200

    Button c-t refactoring

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import s from './Button.module.css';
 
-export default class Button extends Component {
-  render() {
-    const { loadMore } = this.props;
-    return (
-      <button className={s.button} onClick={loadMore} type="button">
-        Load more
-      </button>
-    );
-  }
+export default function Button({ loadMore }) {
+  return (
+    <button className={s.button} onClick={loadMore} type="button">
+      Load more
+    </button>
+  );
 }
+
+Button.propTypes = {
+  loadMore: PropTypes.func.isRequired,
+};
