e18d42aa3dfa0e021eca3e93b01f373402ff7014
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-19 18:53:34 +0200

    ImageGalleryItem c-t refactoring

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component {
-  static propTypes = {
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { webformatURL, largeImageURL, showFull, tags } = this.props;
-    return (
-      <li className={s.item}>
-        <img
-          className={s.image}
-          onClick={() => showFull(largeImageURL)}
-          src={webformatURL}
-          alt={tags}
-        />
-      </li>
-    );
-  }
+export default function ImageGalleryItem({
+  webformatURL,
+  largeImageURL,
+  showFull,
+  tags,
+}) {
+  return (
+    <li className={s.item}>
+      <img
+        className={s.image}
+        onClick={() => showFull(largeImageURL)}
+        src={webformatURL}
+        alt={tags}
+      />
+    </li>
+  );
 }
 
-export default ImageGalleryItem;
+ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  showFull: PropTypes.func.isRequired,
+  tags: PropTypes.string.isRequired,
+};
