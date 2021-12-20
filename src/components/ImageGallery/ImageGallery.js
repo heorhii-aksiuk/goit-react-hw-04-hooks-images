c019ed2cf9463faa09f5901c3dcc8a536e94ddea
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-20 17:37:56 +0200

    ImageGallery c-t refactoring

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import s from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  static propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-      }),
-    ),
-  };
-
-  render() {
-    const { items, showFull } = this.props;
-    return (
-      <ul className={s.imageGallery}>
-        {items.map(item => (
-          <ImageGalleryItem
-            key={item.id}
-            showFull={showFull}
-            webformatURL={item.webformatURL}
-            largeImageURL={item.largeImageURL}
-            tags={item.tags}
-          />
-        ))}
-      </ul>
-    );
-  }
+export default function ImageGallery({ items, showFull }) {
+  return (
+    <ul className={s.imageGallery}>
+      {items.map(item => (
+        <ImageGalleryItem
+          key={item.id}
+          showFull={showFull}
+          webformatURL={item.webformatURL}
+          largeImageURL={item.largeImageURL}
+          tags={item.tags}
+        />
+      ))}
+    </ul>
+  );
 }
 
-export default ImageGallery;
+ImageGallery.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
