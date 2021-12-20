4f0d99292d47413a7ecd5ea8a73dc6c3eacf7fe4
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-20 17:43:30 +0200

    Modal c-t refactoring

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
-export default class Modal extends Component {
-  render() {
-    const { closeModal, children } = this.props;
-    return (
-      <div onClick={closeModal} className={s.overlay}>
-        <div className={s.modal}>{children}</div>
-      </div>
-    );
-  }
+export default function Modal({ closeModal, children }) {
+  return (
+    <div onClick={closeModal} className={s.overlay}>
+      <div className={s.modal}>{children}</div>
+    </div>
+  );
 }
+
+Modal.propTypes = {
+  closeModal: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+};
